5284f939ff3bd578f746723a60054b5179dc4853
sagi <smauda85@gmail.com>
2022-07-18 12:24:35 +0300

    Initial commit

---
+++ src/main/java/com/example/PupilRegistration/PupilRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.PupilRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PupilRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PupilRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PupilRegistration/PupilRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PupilRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PupilRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
